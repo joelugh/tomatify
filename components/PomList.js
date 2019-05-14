02af175fd62899dac514e93ffbd93e9b9a81ff66
hugh <hughetchan@gmail.com>
2019-05-14 11:59:40 +1000

    Added expand all toggle to PomList

--- components/PomList.js
+++ components/PomList.js
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListSubheader from '@material-ui/core/ListSubheader';
+import Button from '@material-ui/core/Button';
+import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore';
+import UnfoldLessIcon from '@material-ui/icons/UnfoldLess';
 
 import Pom from './Pom';
 
@@ -14,12 +17,22 @@ const styles = theme => ({
         minWidth: '360px',
         backgroundColor: theme.palette.background.paper,
     },
+    subheader: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        paddingRight: 0,
+    },
+    toggle: {
+        height: 40,
+        minWidth: 40,
+    }
 });
 
 class PomList extends React.Component {
 
     state = {
         expanded: null,
+        all: false,
     }
 
     componentDidUpdate(prevProps) {
@@ -27,13 +40,20 @@ class PomList extends React.Component {
     }
 
     handleExpand = idx => this.setState(state => ({
-        expanded: state.expanded === idx ? null : idx,
+        all: false,
+        expanded: state.all ? null : state.expanded === idx ? null : idx,
+    }));
+
+    handleToggle = () => this.setState(state => ({
+        all: !(state.all || state.expanded !== null),
+        expanded: null,
     }));
 
     render() {
 
         const {
             expanded,
+            all,
         } = this.state;
 
         const {
@@ -50,14 +70,26 @@ class PomList extends React.Component {
             onSync,
         } = this.props;
 
-        return <List className={classes.root} subheader={<ListSubheader>{subheaderText} ({poms.length})</ListSubheader>} >
+        const unfoldLess = all || expanded !== null;
+
+        return <List
+            className={classes.root}
+            subheader={
+                <ListSubheader className={classes.subheader}>
+                    <span>{subheaderText} ({poms.length})</span>
+                    <Button className={classes.toggle} onClick={this.handleToggle}>
+                        {unfoldLess ? <UnfoldLessIcon /> : <UnfoldMoreIcon />}
+                    </Button>
+                </ListSubheader>
+            }
+        >
             {poms.map((_pom, idx) => {
                 const { id, uri, ...pom } = _pom;
                 const isFavourite = favourites[id];
                 return <Pom
                     key={id}
                     {...pom}
-                    expanded={expanded === idx}
+                    expanded={all || expanded === idx}
                     handleExpand={() => this.handleExpand(idx)}
                     isFavourite={isFavourite}
                     onClick={() => onClick(id)}

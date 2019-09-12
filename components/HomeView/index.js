dff7ffba4fb004290e1704ea240f9722eb890a1f
hugh <hughetchan@gmail.com>
2019-09-12 22:44:11 +1000

    Added some redux state to track landing page status

--- components/HomeView/index.js
+++ components/HomeView/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { compose } from 'redux'
+import { compose, bindActionCreators } from 'redux';
 import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase';
+
+import {setShowLanding} from '../../redux/client';
 
 import Landing from '../Landing';
 import Poms from '../Poms';
 
 function Home({
-    recent = {}, popular = {}, user, filter, auth, initialised,
+    recent = {}, popular = {}, user, filter, auth, initialised, showLanding, setShowLanding,
 }) {
     const isUser = user != null && isLoaded(auth) && !isEmpty(auth);
-    const [isLanding, setIsLanding] = React.useState(true);
+
+    const [isLanding, setIsLanding] = React.useState(showLanding);
 
     React.useEffect(() => {
-        if (isUser) setIsLanding(false);
-    });
+        if (!showLanding || isUser) setIsLanding(false);
+        setShowLanding(false);
+    },[isUser]);
 
     if (isLanding) return <Landing goToHome={() => setIsLanding(false)}/>;
 
@@ -33,14 +37,17 @@ export default compose(
         'recent',
         'tags',
     ])),
-    connect((state, _props) => ({
-        popular: state.firebase.data.popular,
-        recent: state.firebase.data.recent,
-        auth: state.firebase.auth,
-        user: state.firebase.profile,
-        tags: state.firebase.data.tags,
-        tag: state.client.tag,
-        filter: state.client.filter,
-        initialised: state.client.initialised,
-    }))
+    connect(
+        (state, _props) => ({
+            popular: state.firebase.data.popular,
+            recent: state.firebase.data.recent,
+            auth: state.firebase.auth,
+            user: state.firebase.profile,
+            tags: state.firebase.data.tags,
+            tag: state.client.tag,
+            filter: state.client.filter,
+            showLanding: state.client.showLanding,
+        }),
+        (dispatch) => bindActionCreators({setShowLanding}, dispatch)
+    )
 )(Home);
--- redux/client.js
+++ redux/client.js
@@ -2,12 +2,14 @@ const SET_INITIALISED = 'CLIENT/SET_INITIALISED';
 const SET_TAG = 'CLIENT/SET_TAG';
 const SET_FILTER = 'CLIENT/SET_FILTER';
 const SET_POM_ID = 'CLIENT/SET_POM_ID';
+const SET_SHOW_LANDING = 'CLIENT/SET_SHOW_LANDING';
 
 export const clientInitialState = {
   initialised: false,
   tag: null,
   pomId: null,
   filter: 'home',
+  showLanding: true,
 };
 
 export const setInitialised = bool => {
@@ -38,6 +40,13 @@ export const setFilter = filter => {
   }
 };
 
+export const setShowLanding = bool => {
+  return {
+    type: SET_SHOW_LANDING,
+    payload: bool,
+  }
+}
+
 export default (state = clientInitialState, {type, payload}) => {
   switch (type) {
     case SET_INITIALISED:
@@ -48,6 +57,8 @@ export default (state = clientInitialState, {type, payload}) => {
       return {...state, filter: payload};
     case SET_POM_ID:
       return {...state, pomId: payload};
+    case SET_SHOW_LANDING:
+      return {...state, showLanding: payload};
     default:
       return state;
   }
